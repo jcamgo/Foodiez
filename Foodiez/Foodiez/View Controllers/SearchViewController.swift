c58cddf59ca86c8f53f8e1c4f7616e2232ab0591
Jonathan Orellana <jonnycamargo96@gmail.com>
2017-10-10 16:13:13 -0600

    Add SearchViewController

---
+++ Foodiez/Foodiez/View Controllers/SearchViewController.swift
@@ -0,0 +1,24 @@
+//
+//  SearchViewController.swift
+//  Foodiez
+//
+//  Created by jonathan orellana on 10/10/17.
+//  Copyright © 2017 jonathan orellana. All rights reserved.
+//
+
+import UIKit
+import
+
+class SearchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
